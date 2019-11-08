3c9db830005f363d224451a0659bce6017297839
mejiacampos <marvin.campos2000@gmail.com>
2019-11-08 22:58:58 +0100

    conectados

--- app/src/main/java/com/example/proyecto_final2019/MainActivity.java
+++ app/src/main/java/com/example/proyecto_final2019/MainActivity.java
@@ -8,6 +8,7 @@ public class MainActivity extends AppCompatActivity {
 
     //comentario
 
+    //estamos conectados
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
