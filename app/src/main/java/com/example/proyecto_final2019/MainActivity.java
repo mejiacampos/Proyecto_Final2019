44604d28779306de47e00053bde9e9b98a08c808
Daniel Dominguez <dd868759@gmail.com>
2019-11-14 13:34:04 -0600

    modificando MainActivity

--- app/src/main/java/com/example/proyecto_final2019/MainActivity.java
+++ app/src/main/java/com/example/proyecto_final2019/MainActivity.java
@@ -1,32 +1,37 @@
 package com.example.proyecto_final2019;
 
-import android.content.ContentValues;
-import android.content.Intent;
-import android.database.Cursor;
-import android.database.sqlite.SQLiteDatabase;
-import android.os.Bundle;
-
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-
-import androidx.appcompat.app.AlertDialog;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
 
-import android.view.View;
+import android.content.Intent;
+import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-//dani
+    EditText Emarca, Emodelo;
+    Button guardar,buscar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-    }
-}
-
+        setContentView(R.layout.activity_main);
+        Emarca=(EditText)findViewById(R.id.marca);
+        Emodelo=(EditText)findViewById(R.id.modelo);
+        guardar=(Button)findViewById(R.id.guardar);
+        buscar=(Button)findViewById(R.id.buscar);
+        guardar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DB db= new DB(getApplicationContext(),null,null,1);
+                String marca = Emarca.getText().toString();
+                String modelo = Emodelo.getText().toString();
+                String mensaje =db.guardar(marca, modelo);
+                Toast.makeText(getApplicationContext(),mensaje,Toast.LENGTH_SHORT).show();
+            }
+        });
