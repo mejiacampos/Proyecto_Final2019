ad0af098fde09f49f0ea661c490f9ee9d9b24f1c
mejiacampos <marvin.campos2000@gmail.com>
2019-11-14 18:32:05 +0100

    agrege estilo de boton, imajen y modifique el values dimens

--- app/src/main/java/com/example/proyecto_final2019/MainActivity.java
+++ app/src/main/java/com/example/proyecto_final2019/MainActivity.java
@@ -1,10 +1,24 @@
 package com.example.proyecto_final2019;
 
-
+import android.content.ContentValues;
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 
-import androidx.appcompat.app.AppCompatActivity;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
 
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
