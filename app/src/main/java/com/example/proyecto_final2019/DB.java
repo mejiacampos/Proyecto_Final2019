2c8ed194a0956e7339c9813724bbb15ce10ba051
Daniel Dominguez <dd868759@gmail.com>
2019-11-14 14:51:58 -0600

    codigo de la clase DB

--- app/src/main/java/com/example/proyecto_final2019/DB.java
+++ app/src/main/java/com/example/proyecto_final2019/DB.java
@@ -1,4 +1,43 @@
 package com.example.proyecto_final2019;
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
 
-public class DB {
-}
+import androidx.annotation.Nullable;
+
+import java.util.ArrayList;
+
+public class DB extends SQLiteOpenHelper {
+    public DB(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, "prueba", factory, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("create table datos(marca text, modelo text, color text, año text, precio text)");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+
+    public String guardar(String marca, String modelo){
+        String mensaje="'";
+        SQLiteDatabase database = this.getWritableDatabase();
+        ContentValues contenedor = new ContentValues();
+        contenedor.put("marca",marca);
+        contenedor.put("modelo",modelo);
+        try {
+            database.insertOrThrow("datos",null,contenedor);
+            mensaje="Ingresado Correctamente";
+        }catch (SQLException e) {
+            mensaje="No Ingresado";
+        }
+
+
+        return mensaje;
+    }
